008ce4c828f19cedd641907d0377331f6c9ec9da
Nishant Aher <nishantaher007@gmail.com>
2025-12-18 17:03:22 +0530

    Create lowest_common_ancestor.py

---
+++ binary_trees/lowest_common_ancestor.py
@@ -0,0 +1,16 @@
+# Problem: Lowest Common Ancestor of a Binary Tree
+# Approach: return node if it is p or q- if both left and right return non-null, current node is the LCA.
+# Time Complexity:O(n)
+# Space Complexity: O(n)
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root or root == p or root == q:
+            return root
+
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+
+        if left and right:
+            return root
+    
+        return left or right
