d9de4a91c52121fa563a87e25db2a4f1fb195ff6
Nishant Aher <nishantaher007@gmail.com>
2026-01-01 16:09:16 +0530

    Create house_robber_ii.py

---
+++ dynamic_programming/house_robber_ii.py
@@ -0,0 +1,17 @@
+# Problem: House Robber ii
+# Approach: Split into two linear robberies excluding first or last house.
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        return max(nums[0], self.helper(nums[1:]), self.helper(nums[:-1]))
+
+    def helper(self, nums):
+        rob1, rob2 = 0, 0
+
+        for n in nums:
+            newRob = max(rob1 + n, rob2)
+            rob1 = rob2
+            rob2 = newRob
+        return rob2
